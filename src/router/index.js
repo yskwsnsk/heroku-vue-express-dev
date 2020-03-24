51708af9818c93a8afb3bb42e94bc6c2c463df4a
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-24 15:21:27 +0900

    a

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import HelloWorld from '@/components/HelloWorld'
+import Top from '@/components/Top'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
@@ -17,6 +19,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/hello',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/',
+    name: 'Top',
+    component: Top
   }
 ]
 

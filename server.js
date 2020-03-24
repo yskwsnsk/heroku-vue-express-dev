7dae336c288581ba90d7f95e9456058d479152fd
吉川　峻策 <yoshikawashunsaku@maedanagisa-no-MacBook-Air.local>
2020-03-24 11:33:37 +0900

    a

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 // server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.static(__dirname + "/dist/"));
 app.listen(port);
-console.log('server started '+ port);
